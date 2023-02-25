ece7da903db93c280b8c41449b551e8417b78c8a
mathesukkj <kemuel.g7363@gmail.com>
2023-02-25 18:52:28 -0300

    feat: add Installment toString

--- src/entities/Installment.java
+++ src/entities/Installment.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Installment {
     private LocalDate dueDate;
@@ -25,6 +26,11 @@ public class Installment {
 
     public void setAmount(double amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return dueDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + " - " + amount;
     }
 
 }
