9b999cce122fb21fe35b65c8db47bf629c972070
mathesukkj <kemuel.g7363@gmail.com>
2023-02-25 18:17:27 -0300

    feat: add services

---
+++ src/services/ContractService.java
@@ -0,0 +1,9 @@
+package services;
+
+import entities.Contract;
+
+public class ContractService {
+    public void processContract(Contract contract, int number) {
+
+    }
+}
---
+++ src/services/PaypalService.java
@@ -0,0 +1,13 @@
+package services;
+
+public class PaypalService implements OnlinePaymentService {
+
+    public double paymentFee(double amount) {
+        throw new UnsupportedOperationException("Unimplemented method 'paymentFee'");
+    }
+
+    public double interest(double amount, int months) {
+        throw new UnsupportedOperationException("Unimplemented method 'interest'");
+    }
+
+}
